8aeef6a4bf0d175b0c234bb81c93dd00a9eea587
Zlatko Stamatov <Zlatko.Stamatov@cariad.technology>
2022-12-01 08:55:26 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,34 @@
 fun main() {
+    fun getElves(input: List<String>): ArrayList<Int> {
+        val elves = arrayListOf<Int>()
+        var elve = 0
+        input.forEach {
+            if (it == "") {
+                elves.add(elve)
+                elve = 0
+            } else {
+                elve += it.toInt()
+            }
+        }
+        elves.add(elve)
+        return elves
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return getElves(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getElves(input)
+            .sortedByDescending { it }
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
