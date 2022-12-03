63fbec1e02dbe675cec5058e6a2bd2ac490a9c7a
Zlatko Stamatov <Zlatko.Stamatov@cariad.technology>
2022-12-03 06:55:49 +0100

    Day 3

---
+++ src/Day03.kt
@@ -0,0 +1,34 @@
+fun main() {
+
+    fun getIntValue(it: Char) = if (it.isLowerCase()) it.toInt() - 96 else it.toInt() - 38
+
+    fun getValue1(line: String): Int {
+        val substring1 = line.substring(0, line.length / 2)
+        val substring2 = line.substring(line.length / 2)
+        substring1.forEach { if (substring2.contains(it)) return getIntValue(it) }
+        throw java.lang.RuntimeException()
+    }
+
+    fun part1(input: List<String>): Int {
+        return input.sumOf { getValue1(it) }
+    }
+
+    fun getValue2(strings: List<String>): Int {
+        strings[0].forEach { if (strings[1].contains(it) && strings[2].contains(it)) return getIntValue(it) }
+        throw java.lang.RuntimeException()
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.chunked(3).sumOf { getValue2(it) }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    println(part1(testInput))
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
+}
