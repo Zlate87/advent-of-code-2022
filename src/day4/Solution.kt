8dcae731ebde87d4a5be93b91a49fc030adb89b8
Zlatko Stamatov <Zlatko.Stamatov@cariad.technology>
2022-12-04 09:38:55 +0100

    Day 4 - Quick&Dirty

--- src/day4/Solution.kt
+++ src/day4/Solution.kt
@@ -4,18 +4,44 @@ import readInput
 
 fun main() {
 
+    fun temp(pair: String) = pair.split("-").map { it.toInt() }
+
+    fun temo2(firstElf: List<Int>): Set<Int> {
+        val set = HashSet<Int>()
+        for (i in firstElf[0]..firstElf[1]) {
+            set.add(i)
+        }
+        return set
+    }
+
+    fun temp(input: List<String>) = input
+        .map { pairString -> pairString.split(",") }
+        .map { pairList ->
+            val firstElf = temp(pairList[0])
+            val secondElf = temp(pairList[1])
+            val firstSet = temo2(firstElf)
+            val secondSet = temo2(secondElf)
+            Pair(firstSet, secondSet)
+        }
+
     fun part1(input: List<String>): Int {
-        return -1
+        return temp(input)
+            .count {
+                it.first.containsAll(it.second) or it.second.containsAll(it.first)
+            }
     }
 
     fun part2(input: List<String>): Int {
-        return -1
+        return temp(input)
+            .count {
+                it.first.intersect(it.second).isNotEmpty()
+            }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day4/Input_test")
-    check(part1(testInput) == -1)
-    check(part2(testInput) == -1)
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInput("day4/Input")
     println(part1(input))
